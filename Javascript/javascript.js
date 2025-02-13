aad0b2e9b88229671f16cb12e37a7e79c814424e
TeenKiummo <72925631+TeenKiummo@users.noreply.github.com>
2025-02-13 22:25:16 +0100

    asd

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -44,7 +44,15 @@ document.addEventListener("DOMContentLoaded", function () {
     slides[slideIndex - 1].classList.add("active");
     startSlideshow(); // Riavvia il timer
   }
-
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navbar = document.querySelector(".navbar");
+  
+    menuIcon.addEventListener("click", function () {
+      navbar.classList.toggle("show");
+    });
+  });
+  
   // Associa le frecce ai controlli manuali
   document.querySelector(".prev").addEventListener("click", () => manualChangeSlide(-1));
   document.querySelector(".next").addEventListener("click", () => manualChangeSlide(1));
