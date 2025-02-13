c748c3aec8a2149b13c255e65d66ac66440b14c6
TeenKiummo <72925631+TeenKiummo@users.noreply.github.com>
2025-02-13 21:05:50 +0100

    DESKTOP FINE

--- Javascript/gallery.js
+++ Javascript/gallery.js
@@ -1,32 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Aggiungi l'evento di apertura della modale per tutte le immagini
-    const galleryImages = document.querySelectorAll(".gallery-item img");
-    galleryImages.forEach(img => {
-      img.addEventListener("click", function() {
-        openModal(img);
-      });
-    });
-  
-    // Funzione per aprire la modale e visualizzare l'immagine
-    function openModal(imgElement) {
-      var modal = document.getElementById("imageModal");
-      var modalImg = document.getElementById("modalImage");
-  
-      modal.style.display = "flex"; // Mostra la modale
-      modalImg.src = imgElement.src; // Imposta l'immagine nella modale
-    }
-  
-    // Funzione per chiudere la modale
-    function closeModal() {
-      var modal = document.getElementById("imageModal");
-      modal.style.display = "none"; // Nasconde la modale
-    }
-  
-    // Associa l'evento di chiusura della modale (cliccando sullo sfondo)
-    document.querySelector(".modal").addEventListener("click", function(event) {
-      if (event.target === document.querySelector(".modal")) {
-        closeModal(); // Chiude la modale se si clicca sullo sfondo
-      }
+  // Aggiungi l'evento di apertura della modale per tutte le immagini
+  const galleryImages = document.querySelectorAll(".gallery-item img");
+  galleryImages.forEach(img => {
+    img.addEventListener("click", function() {
+      openModal(img);
     });
   });
-  
+
+  // Funzione per aprire la modale e visualizzare l'immagine
+  function openModal(imgElement) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImage");
+
+    modal.style.display = "flex"; // Mostra la modale
+    modalImg.src = imgElement.src; // Imposta l'immagine nella modale
+  }
+
+  // Funzione per chiudere la modale
+  function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none"; // Nasconde la modale
+  }
+
+  // Associa l'evento di chiusura della modale (cliccando sullo sfondo)
+  document.querySelector(".modal").addEventListener("click", function(event) {
+    if (event.target === document.querySelector(".modal")) {
+      closeModal(); // Chiude la modale se si clicca sullo sfondo
+    }
+  });
+
+  // Aggiungi un evento di clic sulla X per chiudere la modale
+  const closeButton = document.querySelector(".modal .close");
+  if (closeButton) {
+    closeButton.addEventListener("click", closeModal); // Chiude la modale quando si clicca sulla X
+  }
+});
