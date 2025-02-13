cb4b2607275710cfcf41fc16c8198d00a1e2cd38
TeenKiummo <72925631+TeenKiummo@users.noreply.github.com>
2025-02-13 17:18:50 +0100

    sd

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,21 +1,32 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.querySelectorAll(".slide");
-  slides.forEach(slide => slide.classList.remove("active")); // Rimuovi la classe active da tutte le immagini
-  
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  
-  slides[slideIndex - 1].classList.add("active"); // Aggiungi la classe active all'immagine corrente
-  setTimeout(showSlides, 3000); // Cambia immagine ogni 3 secondi
-}
-
-function toggleMenu() {
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
     const navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("show");
-  }
+  
+    menuIcon.addEventListener("click", function () {
+      navbar.classList.toggle("show");
+    });
+
+    let slideIndex = 0;
+    showSlides();
+    
+    function showSlides() {
+      let slides = document.querySelectorAll(".slide");
+      slides.forEach(slide => slide.classList.remove("active")); // Rimuovi la classe active da tutte le immagini
+      
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      
+      slides[slideIndex - 1].classList.add("active"); // Aggiungi la classe active all'immagine corrente
+      setTimeout(showSlides, 3000); // Cambia immagine ogni 3 secondi
+    }
+    
+    function toggleMenu() {
+        const navbar = document.querySelector(".navbar");
+        navbar.classList.toggle("show");
+      }
+      
+
+  });
   
